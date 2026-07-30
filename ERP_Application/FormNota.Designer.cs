fe2363ebf47bbf5092f129c013ad226c5bc07091
EDBERT FEDERICO <s160425095@microsoft.ubaya.ac.id>
2026-07-30 16:30:22 +0700

    Printing edit

--- ERP_Application/FormNota.Designer.cs
+++ ERP_Application/FormNota.Designer.cs
@@ -29,13 +29,23 @@
         private void InitializeComponent()
         {
             this.dataGridViewNota = new System.Windows.Forms.DataGridView();
+            this.labelTanggal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
             this.colNomor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colNamabarang = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colBanyaknya = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colSatuan = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.colHarga = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colJumlah = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNota)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewNota
@@ -43,6 +53,7 @@
             this.dataGridViewNota.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewNota.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewNota.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewNota.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colNomor,
@@ -55,78 +66,181 @@
             this.dataGridViewNota.Name = "dataGridViewNota";
             this.dataGridViewNota.RowHeadersWidth = 51;
             this.dataGridViewNota.RowTemplate.Height = 24;
-            this.dataGridViewNota.Size = new System.Drawing.Size(750, 207);
+            this.dataGridViewNota.Size = new System.Drawing.Size(922, 272);
             this.dataGridViewNota.TabIndex = 0;
             this.dataGridViewNota.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewNota_CellValueChanged);
             this.dataGridViewNota.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridViewNota_RowsAdded);
             this.dataGridViewNota.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.dataGridViewNota_RowsRemoved);
             // 
+            // labelTanggal
+            // 
+            this.labelTanggal.AutoSize = true;
+            this.labelTanggal.Location = new System.Drawing.Point(698, 13);
+            this.labelTanggal.Name = "labelTanggal";
+            this.labelTanggal.Size = new System.Drawing.Size(208, 16);
+            this.labelTanggal.TabIndex = 1;
+            this.labelTanggal.Text = "Tanggal 21 Agustus 2026 00:00:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(698, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kepada YTH:";
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(793, 48);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(147, 22);
+            this.textBoxNama.TabIndex = 3;
+            this.textBoxNama.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxNama_KeyDown);
+            // 
             // colNomor
             // 
+            this.colNomor.FillWeight = 57.23598F;
             this.colNomor.HeaderText = "No.";
             this.colNomor.MinimumWidth = 6;
             this.colNomor.Name = "colNomor";
-            this.colNomor.Width = 70;
             // 
             // colNamabarang
             // 
+            this.colNamabarang.FillWeight = 160.4279F;
             this.colNamabarang.HeaderText = "NAMA BARANG";
             this.colNamabarang.MinimumWidth = 6;
             this.colNamabarang.Name = "colNamabarang";
-            this.colNamabarang.Width = 125;
             // 
             // colBanyaknya
             // 
+            this.colBanyaknya.FillWeight = 95.58409F;
             this.colBanyaknya.HeaderText = "BANYAKNYA";
             this.colBanyaknya.MinimumWidth = 6;
             this.colBanyaknya.Name = "colBanyaknya";
-            this.colBanyaknya.Width = 125;
             // 
             // colSatuan
             // 
+            this.colSatuan.FillWeight = 95.58409F;
             this.colSatuan.HeaderText = "SATUAN";
             this.colSatuan.MinimumWidth = 6;
             this.colSatuan.Name = "colSatuan";
-            this.colSatuan.Width = 125;
             // 
             // colHarga
             // 
+            this.colHarga.FillWeight = 95.58409F;
             this.colHarga.HeaderText = "HARGA";
             this.colHarga.MinimumWidth = 6;
             this.colHarga.Name = "colHarga";
-            this.colHarga.Width = 125;
             // 
             // colJumlah
             // 
+            this.colJumlah.FillWeight = 95.58409F;
             this.colJumlah.HeaderText = "JUMLAH";
             this.colJumlah.MinimumWidth = 6;
             this.colJumlah.Name = "colJumlah";
-            this.colJumlah.Width = 125;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 436);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tanda Terima";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(694, 436);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hormat Kami,";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 509);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "_______________";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(694, 520);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "_______________";
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.BackColor = System.Drawing.Color.IndianRed;
+            this.buttonPrint.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonPrint.Location = new System.Drawing.Point(779, 607);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(127, 39);
+            this.buttonPrint.TabIndex = 8;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = false;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 102);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
             // 
             // FormNota
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(946, 658);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTanggal);
             this.Controls.Add(this.dataGridViewNota);
             this.Name = "FormNota";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormNota";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FormNota_Load);
+            this.Shown += new System.EventHandler(this.FormNota_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNota)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridViewNota;
+        private System.Windows.Forms.Label labelTanggal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNama;
         private System.Windows.Forms.DataGridViewTextBoxColumn colNomor;
         private System.Windows.Forms.DataGridViewTextBoxColumn colNamabarang;
         private System.Windows.Forms.DataGridViewTextBoxColumn colBanyaknya;
         private System.Windows.Forms.DataGridViewComboBoxColumn colSatuan;
         private System.Windows.Forms.DataGridViewTextBoxColumn colHarga;
         private System.Windows.Forms.DataGridViewTextBoxColumn colJumlah;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- ERP_Application/FormNota.cs
+++ ERP_Application/FormNota.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace ERP_Application
 {
@@ -31,6 +32,9 @@ namespace ERP_Application
         private void FormNota_Load(object sender, EventArgs e)
         {
             frmMain = (Form1)this.Owner;
+            labelTanggal.Text = "Tanggal " + DateTime.Now;
+            dataGridViewNota.Columns[0].ReadOnly = true; // No.
+            dataGridViewNota.Columns[5].ReadOnly = true; // JUMLAH
         }
 
         private void dataGridViewNota_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
@@ -69,6 +73,125 @@ namespace ERP_Application
         private void dataGridViewNota_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
         {
             UpdateRowNumbers();
+        }
+
+        private void textBoxNama_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // Prevents the beep sound
+
+                // Move focus directly to the DataGridView's first item cell (NAMA BARANG)
+                dataGridViewNota.Focus();
+                if (dataGridViewNota.Rows.Count > 0)
+                {
+                    dataGridViewNota.CurrentCell = dataGridViewNota.Rows[0].Cells[1]; // Column 1 = NAMA BARANG
+                }
+            }
+        }
+
+        private void FormNota_Shown(object sender, EventArgs e)
+        {
+            // Highlighting / focusing the Name text box immediately
+            textBoxNama.Focus();
+            textBoxNama.SelectAll();
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            // 1. Create a PrintDocument object
+            PrintDocument printDoc = new PrintDocument();
+
+            // 2. Attach the PrintPage event handler
+            printDoc.PrintPage += new PrintPageEventHandler(PrintDocument_PrintPage);
+
+            // 3. Show standard Print Preview dialog (or direct print)
+            PrintPreviewDialog previewDialog = new PrintPreviewDialog();
+            previewDialog.Document = printDoc;
+
+            // Maximize preview window for easy view
+            ((Form)previewDialog).WindowState = FormWindowState.Maximized;
+            previewDialog.ShowDialog();
+        }
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Font fontHeader = new Font("Arial", 14, FontStyle.Bold);
+            Font fontSubHeader = new Font("Arial", 10, FontStyle.Regular);
+            Font fontBody = new Font("Arial", 9, FontStyle.Regular);
+            Font fontBodyBold = new Font("Arial", 9, FontStyle.Bold);
+
+            int startX = 40;
+            int startY = 40;
+            int offsetY = 0;
+
+            // --- HEADER SECTION ---
+            g.DrawString("NOTA PENJUALAN", fontHeader, Brushes.Black, startX, startY + offsetY);
+            offsetY += 25;
+
+            g.DrawString("Tanggal : " + DateTime.Now.ToString("dd/MM/yyyy HH:mm"), fontSubHeader, Brushes.Black, startX, startY + offsetY);
+            offsetY += 20;
+
+            g.DrawString("Kepada YTH : " + textBoxNama.Text, fontSubHeader, Brushes.Black, startX, startY + offsetY);
+            offsetY += 30;
+
+            // --- TABLE HEADERS ---
+            g.DrawString("NO.", fontBodyBold, Brushes.Black, startX, startY + offsetY);
+            g.DrawString("NAMA BARANG", fontBodyBold, Brushes.Black, startX + 40, startY + offsetY);
+            g.DrawString("BANYAKNYA", fontBodyBold, Brushes.Black, startX + 260, startY + offsetY);
+            g.DrawString("SATUAN", fontBodyBold, Brushes.Black, startX + 360, startY + offsetY);
+            g.DrawString("HARGA", fontBodyBold, Brushes.Black, startX + 440, startY + offsetY);
+            g.DrawString("JUMLAH", fontBodyBold, Brushes.Black, startX + 540, startY + offsetY);
+
+            offsetY += 15;
+            g.DrawLine(Pens.Black, startX, startY + offsetY, startX + 650, startY + offsetY);
+            offsetY += 10;
+
+            // --- TABLE DATA ROWS ---
+            decimal grandTotal = 0;
+
+            for (int i = 0; i < dataGridViewNota.Rows.Count; i++)
+            {
+                var row = dataGridViewNota.Rows[i];
+                if (row.IsNewRow) continue; // Skip empty row at bottom
+
+                string no = row.Cells[0].Value?.ToString() ?? "";
+                string nama = row.Cells[1].Value?.ToString() ?? "";
+                string banyak = row.Cells[2].Value?.ToString() ?? "";
+                string satuan = row.Cells[3].Value?.ToString() ?? "";
+                string harga = row.Cells[4].Value?.ToString() ?? "";
+                string jumlah = row.Cells[5].Value?.ToString() ?? "";
+
+                // Calculate Grand Total safely
+                decimal rowTotal = 0;
+                decimal.TryParse(jumlah.Replace(".", ""), out rowTotal);
+                grandTotal += rowTotal;
+
+                // Draw Row Items
+                g.DrawString(no, fontBody, Brushes.Black, startX, startY + offsetY);
+                g.DrawString(nama, fontBody, Brushes.Black, startX + 40, startY + offsetY);
+                g.DrawString(banyak, fontBody, Brushes.Black, startX + 260, startY + offsetY);
+                g.DrawString(satuan, fontBody, Brushes.Black, startX + 360, startY + offsetY);
+                g.DrawString(harga, fontBody, Brushes.Black, startX + 440, startY + offsetY);
+                g.DrawString(jumlah, fontBody, Brushes.Black, startX + 540, startY + offsetY);
+
+                offsetY += 20;
+            }
+
+            // --- FOOTER & TOTAL ---
+            g.DrawLine(Pens.Black, startX, startY + offsetY, startX + 650, startY + offsetY);
+            offsetY += 10;
+
+            g.DrawString("TOTAL: Rp " + grandTotal.ToString("N0"), fontBodyBold, Brushes.Black, startX + 440, startY + offsetY);
+            offsetY += 50;
+
+            // Signatures
+            g.DrawString("Tanda Terima,", fontBody, Brushes.Black, startX + 50, startY + offsetY);
+            g.DrawString("Hormat Kami,", fontBody, Brushes.Black, startX + 450, startY + offsetY);
+
+            offsetY += 60;
+            g.DrawString("( _______________ )", fontBody, Brushes.Black, startX + 30, startY + offsetY);
+            g.DrawString("( _______________ )", fontBody, Brushes.Black, startX + 430, startY + offsetY);
         }
     }
 }
