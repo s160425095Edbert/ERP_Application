1daf4f116c2a09c488d5491a6713669e91f910cc
EDBERT FEDERICO <s160425095@microsoft.ubaya.ac.id>
2026-08-01 17:01:45 +0700

    Verification password owner

--- ERP_Application/FormAdmin.Designer.cs
+++ ERP_Application/FormAdmin.Designer.cs
@@ -68,7 +68,9 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormAdmin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- ERP_Application/Owner.cs
+++ ERP_Application/Owner.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Org.BouncyCastle.Tls;
 
 namespace ERP_Application
 {
     public class Owner
     {
         private string password;
+        private string username;
+
+        public Owner(string password)
+        {
+            this.Password = password;
+            this.Username = "Owner";
+        }
+
+        public string Password {private get => password; set => password = value; }
+        public string Username { get => username; set => username = value; }
+
+        public bool verifyPassword(string verify)
+        {
+            if(verify == password)
+                return true;
+            else
+                return false;
+        }
+        public void ChangePassword(string newPassword)
+        {
+            Password = newPassword;
+        }
     }
 }
